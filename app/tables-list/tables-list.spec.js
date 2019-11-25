8332a3cb6673fea26dc9dfffa2f11097422b7759
dzonsi <nikola86_v@yahoo.com>
2019-11-25 10:30:55 +0100

    change tables-list.spec.js file

--- app/tables-list/tables-list.spec.js
+++ app/tables-list/tables-list.spec.js
@@ -22,34 +22,22 @@ describe('tablesList', function() {
   describe('TablesListController', function() {
 
     it('should create show property with value all', function() {
-      $componentController('tablesList', { $scope: $rootScope});
+      var controller = $componentController('tablesList', { $scope: $rootScope});
+      // call $onInit() to initialize data in controller
+      controller.$onInit();
       $httpBackend.flush();
       expect($rootScope.filter.show).toBe('all');
     });
 
     it('should create scope tables and drinks objects', function() {
       $httpBackend.expectGET('data/tables.json');
-      $componentController('tablesList', { $scope: $rootScope});
+      var controller = $componentController('tablesList', { $scope: $rootScope});
+      // call $onInit() to initialize data in controller
+      controller.$onInit();
       $httpBackend.flush();
       expect($rootScope.tables).toEqual([{name: 'tables'}]);
       expect($rootScope.drinks).toEqual([{name: 'drinks'}]);
     });
-
-    /*it('should add new drink to drinks object', function() {
-      $httpBackend.expectGET('data/drinks.json');
-      $componentController('tablesList', { $scope: $rootScope});
-      $rootScope.addDrink('Ice coffee', 270);
-      $httpBackend.flush();
-      expect($rootScope.drinks).toEqual([{name: 'Ice coffee', price: 270}, {name: 'drinks'}]);
-    });
-
-    it('should add new table to tables object', function() {
-      $httpBackend.expectGET('data/tables.json');
-      $componentController('tablesList', { $scope: $rootScope});
-      $rootScope.addTable(1, false, 0);
-      $httpBackend.flush();
-      expect($rootScope.tables).toEqual([{number:1, occupied: false, amount: 0, orders: []}, {name: 'tables'}]);
-    });*/
 
   });
 
